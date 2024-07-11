64ccd4c842dac547025c0c33bed246a211c6ea8c
Bruno X <oliveirabcx@gmail.com>
2024-07-11 02:03:14 -0300

    index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,48 @@
-const personagens = document.querySelectorAll('.personagem')
+const personagens = document.querySelectorAll('.personagem');
+let isSpacePressed = false;
+
+document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+        isSpacePressed = true;
+        updateCharacter(document.querySelector('.selecionado'));
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    if (event.code === 'Space') {
+        isSpacePressed = false;
+        updateCharacter(document.querySelector('.selecionado'));
+    }
+});
 
 personagens.forEach((personagem) => {
     personagem.addEventListener('mouseenter', () => {
 
         if (window.innerWidth < 450) {
-            window.scrollTo({ top: 0, behavior: "smooth" })
+            window.scrollTo({ top: 0, behavior: "smooth" });
         }
 
         const personagemSelecionado = document.querySelector('.selecionado');
-        personagemSelecionado.classList.remove('selecionado')
+        personagemSelecionado.classList.remove('selecionado');
+        
+        personagem.classList.add('selecionado');
+        updateCharacter(personagem);
+    });
+});
 
-        personagem.classList.add('selecionado')
+function updateCharacter(personagem) {
+    const imagemPersonagemGrande = document.querySelector(".personagem-grande");
+    const idPersonagem = personagem.attributes.id.value;
+    const nomePersonagem = document.getElementById('nome-personagem');
+    const descricaoPersonagem = document.getElementById('descricao-personagem');
 
-        const imagemPersonagemGrande = document.querySelector('.personagem-grande');
-        const idPersonagem = personagem.attributes.id.value;
+    if (idPersonagem === 'iori' && isSpacePressed) {
+        imagemPersonagemGrande.src = `./src/images/png-iori-alt.png`;
+        nomePersonagem.innerText = "Iori Possuído";
+        descricaoPersonagem.innerText = "A transformação de Iori em Orochi ocorre quando ele é dominado pela maldição de Orochi, uma força ancestral e maligna. Seus poderes são amplificados.";
+    } else {
         imagemPersonagemGrande.src = `./src/images/png-${idPersonagem}.png`;
-
-        const nomePersonagem = document.getElementById('nome-personagem');
         nomePersonagem.innerText = personagem.getAttribute('data-name');
-
-        const descricaoPersonagem = document.getElementById('descricao-personagem');
-        descricaoPersonagem.innerText = personagem.getAttribute('data-description')
-
-
-    })
-})
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+    }
+}
